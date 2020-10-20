9844e9be418bda08b9070bc145d44347b3b2872f
gondolax <60988448+gondolax@users.noreply.github.com>
2020-10-20 15:25:03 +0800

    Update safelinkedit.js

--- safelinkedit.js
+++ safelinkedit.js
@@ -23,6 +23,8 @@ var x=Math.floor((Math.random()*2)+ 1);
 var xxx=null;
 if(x=="1"){xxx="online-photo-editor-gratis-terbaik.html"}
 a_to_ve[i].href="https://www.daftarcaramembuat.com/2016/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="2"){xxx="download-aplikasi-edit-foto-terbaik-keren.html"}
+a_to_ve[i].href="https://www.daftarcaramembuat.com/2016/02/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
